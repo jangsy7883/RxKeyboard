2083447f98f39c011767c9cc744c18b5cbdede3e
장석용 <jangsy@thedaybefore.me>
2025-07-23 09:29:56 +0900

    최소버전을 13으로 변경

--- Package.swift
+++ Package.swift
@@ -4,6 +4,9 @@ import PackageDescription
 
 let package = Package(
   name: "RxKeyboard",
+  platforms: [
+    .iOS(.v13) 
+  ],
   products: [
     .library(name: "RxKeyboard", targets: ["RxKeyboard"]),
   ],
